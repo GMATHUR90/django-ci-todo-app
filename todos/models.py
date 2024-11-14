7c59e4a33ab12cca4ec529b697f1c3d52a3c3550
Gaurav Mathur <ecegauravmathur90@gmail.com>
2024-11-14 18:07:50 +0530

    Update models.py

--- todos/models.py
+++ todos/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 class Todo(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, verbose_name='Task Title')
     created_at = models.DateTimeField('Created', auto_now_add=True)
     update_at = models.DateTimeField('Updated', auto_now=True)
-    isCompleted = models.BooleanField(default=False)
+    isCompleted = models.BooleanField('Completed', default=False)
 
     def __str__(self):
         return self.title
+    
+    class Meta:
+        ordering = ['-created_at']  # Orders todos by creation date, newest first
+        verbose_name = 'Todo'
+        verbose_name_plural = 'Todos'
+
+    def get_status(self):
+        """Returns the status of the todo item"""
+        return "Completed" if self.isCompleted else "Pending"
